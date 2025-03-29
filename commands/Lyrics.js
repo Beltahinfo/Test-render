445cff0cca387ce38c83b72da0519688eafe9265
Beltahinfo <beltahhub@gmail.com>
2025-03-29 21:48:07 +0300

    Beltah Tech 254 👻

--- commands/Lyrics.js
+++ commands/Lyrics.js
@@ -1,12 +1,34 @@
 
 const { keith } = require("../keizzah/keith");
 const axios = require("axios");
+const { repondre } = require(__dirname + "/../keizzah/context");
+
+// Common contextInfo configuration
+const getContextInfo = (title = '', userJid = '', thumbnailUrl = '') => ({
+  mentionedJid: [userJid],
+  forwardingScore: 999,
+  isForwarded: true,
+  forwardedNewsletterMessageInfo: {
+    newsletterJid: "120363249464136503@newsletter",
+    newsletterName: "Beltah Tech Updates",
+    serverMessageId: Math.floor(100000 + Math.random() * 900000),
+  },
+  externalAdReply: {
+    showAdAttribution: true,
+    title: title || "𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗨𝗟𝗧𝗜 𝗗𝗘𝗩𝗜𝗖𝗘",
+    body: "𝗜𝘁 𝗶𝘀 𝗻𝗼𝘁 𝘆𝗲𝘁 𝘂𝗻𝘁𝗶𝗹 𝗶𝘁 𝗶𝘀 𝗱𝗼𝗻𝗲🗿",
+    thumbnailUrl: thumbnailUrl || '',
+    sourceUrl: settings.GURL || '',
+    mediaType: 1,
+    renderLargerThumbnail: false
+  }
+});
 
 // Define the command with aliases
 keith({
   nomCom: "lyrics",
   aliases: ["mistari", "lyric"],
-  reaction: '⚔️',
+  reaction: '🛸',
   categorie: "search"
 }, async (dest, zk, params) => {
   const { repondre: sendResponse, arg: commandArgs, ms } = params;
@@ -64,6 +86,10 @@ keith({
       },
       { quoted: ms }
     );
+await zk.sendMessage(dest, {
+        caption: caption, 
+            contextInfo: getContextInfo("𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗨𝗟𝗧𝗜 𝗗𝗘𝗩𝗜𝗖𝗘", senderName, imageBuffer )
+        }, { quoted: ms });
 
   } catch (error) {
     console.error('Error fetching or sending image:', error);
