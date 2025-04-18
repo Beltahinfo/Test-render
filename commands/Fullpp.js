fa12dcd3b20d757f93c87bf72b8671d1d1845bb2
Beltahinfo <beltahhub@gmail.com>
2025-04-18 11:48:54 +0300

    Update Fullpp.js

--- commands/Fullpp.js
+++ commands/Fullpp.js
@@ -1,6 +1,6 @@
 const { keith } = require("../keizzah/keith");
 const { generateProfilePicture } = require("../keizzah/dl/Function");
-const { S_WHATSAPP_NET } = require('@whiskeysockets/baileys');
+const { S_WHATSAPP_NET } = require('gifted-baileys');
 const fs = require("fs");
 
 keith({
